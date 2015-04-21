3f1a89c6f414ef1b344a213c07be1d9d40c0b468
gioppy <tech@giovannibuffa.it>
2015-04-21 13:02:17 +0200

    Support Colorbox module for open the lazyload image in colorbox

--- lazyloadxt.tpl.php
+++ lazyloadxt.tpl.php
@@ -1,2 +1,1 @@
-<img class="lazy" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7" data-src="<?php print $url; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>">
-<noscript><img src="<?php print $url; ?>" width="<?php print $width; ?>" height="<?php print $height; ?>"></noscript>
+<?php print render($element); ?>
